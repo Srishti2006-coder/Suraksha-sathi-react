4db22a5b2078712187b5173f69d8a154243167f8
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-19 11:38:09 +0530

    App.js function App me return navbar , home and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="bg-blue-600 text-white p-10 text-3xl">
-      Suraksha Sathi 🚀
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-900 text-white p-4 text-center">
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
