2634d10c6cf567f7abe5f8a36b27d64f160f4846
Srishti2006-coder <srishtibansal505@gmail.com>
2026-02-21 23:13:52 +0530

    add css to welcome page

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+
+function Welcome() {
+  return (
+
+<div className="welcome">
+
+<h1 className="title">
+SurakshaSathi
+</h1>
+
+<p className="tagline">
+
+Because every journey deserves safety
+
+</p>
+
+<button className="startBtn">
+
+Get Started
+
+</button>
+
+</div>
+
+  );
+}
+
+export default Welcome;
